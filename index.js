1dec1f1ffe799d472f32fa28db3e52715821edb6
Paulo Gustavo <paulogustavo.dev@gmail.com>
2024-10-20 09:33:23 -0300

    fix: code otimization

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var nome = "joao" //Digite o nome que queira entre aspas
-let resultado = nome.split(""); //aqui irá transforma a variavel nome em um array 
-let tamanho = resultado.length //aqui pegará o tamanho do nome
-var i=Number(tamanho)-1 //como o array começa em zero, então tiro 1 do tamanho para percorrer corretamente
-let novonome= resultado[i]
-while(i>0){
-    novonome=novonome+resultado[i-1] //aqui irá percorrer todo o array, e concatenar assim formando o nome
-    i=i-1
+//Old
+
+// var nome = "joao"; //Digite o nome que queira entre aspas
+// let resultado = nome.split(""); //aqui irá transforma a variavel nome em um array
+// let tamanho = resultado.length; //aqui pegará o tamanho do nome
+// var i = Number(tamanho) - 1; //como o array começa em zero, então tiro 1 do tamanho para percorrer corretamente
+// let novonome = resultado[i];
+// while (i > 0) {
+//   novonome = novonome + resultado[i - 1]; //aqui irá percorrer todo o array, e concatenar assim formando o nome
+//   i = i - 1;
+// }
+// console.log(novonome);
+
+const variable = "alex";
+const words = variable.split("");
+let nameInverted = [];
+for (let i = words.length - 1; i >= 0; i--) {
+  nameInverted.push(words[i]);
 }
-console.log(novonome)
+console.log(nameInverted.join(""));
